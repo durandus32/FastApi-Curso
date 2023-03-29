e67bee5950eab53954276261119db1c40e1dea6f
DanielAsus <durandus@outlook.com>
2023-03-29 14:01:32 -0600

    probando rutas y parametros en url

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World!"}
+
+@app.get('/hola')
+def read_root():
+    return {"Hola": "Mundo!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/calculadora")
+def calculadora(num1: float, num2: float):
+    return{'suma' : num1 + num2}
